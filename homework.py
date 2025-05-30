4993d7200245620303076ea1feef3c0e090bf558
ShauryaSuhane <suhaneshaurya26@gmail.com>
2025-05-30 10:20:25 +0530

    homework

---
+++ homework.py
@@ -0,0 +1,15 @@
+Alphabet=str(input("enter a uppercased alphabet through A to G:"))
+if Alphabet == "A":
+    print("the ASCII number is 65")
+if Alphabet == "B":
+    print("the ASCII number is 66") 
+if Alphabet == "C":
+    print("the ASCII number is 67")
+if Alphabet == "D":
+    print("the ASCII number is 68") 
+if Alphabet == "E":
+    print("the ASCII number is 69") 
+if Alphabet == "F":
+    print("the ASCII number is 70") 
+if Alphabet == "G":
+    print("the ASCII number is 71")     
